8f48937cf6499d07e6b4e5c779f218a52e6afafa
thomashenrywebb-cpu <thomashenrywebb@gmail.com>
2026-02-25 11:27:59 +1100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CLIENT_ID = "PUT_CLIENT_ID_HERE"
 CLIENT_SECRET = "PUT_CLIENT_SECRET_HERE"
 
-TOKEN_URL = "https://auth.cartoncloud.com/oauth/token"
+TOKEN_URL = "https://api.cartoncloud.com/uaa/oauth/token"
 API_URL = "https://api.cartoncloud.com/v1/orders"
